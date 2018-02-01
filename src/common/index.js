9921354ec05ec2b9bc8d76b2bf818e332797ed0d
Rafael Paulino <rafaelpaulino@gmail.com>
2018-02-01 08:42:56 -0300

    git subrepo pull src/common
    subrepo:
      subdir:   "src/common"
      merged:   "3e9049b"
    upstream:
      origin:   "https://github.com/rtpaulino/subrepocommon.git"
      branch:   "master"
      commit:   "3e9049b"
    git-subrepo:
      version:  "0.3.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a7ee886"

--- src/common/index.js
+++ src/common/index.js
@@ -1,8 +1,23 @@
-module.exports = {
-    stringClean (s) {
-        if (s === null || s === undefined) {
-            return s;
-        }
-        return ('' + s).toLowerCase().replace(/\s+/g, ' ');
+var isEmpty = function (s) {
+    return s === null || s === undefined || /^\s*$/.test('' + s);
+};
+
+var stringClean = function (s) {
+    if (isEmpty(s)) {
+        return s;
     }
+    return ('' + s).toLowerCase().replace(/\s+/g, ' ');
+};
+
+var spacesToUnderscore = function (s) {
+    if (isEmpty(s)) {
+        return s;
+    }
+    return ('' + s).replaceAll(/\s/g, '_');
+}
+
+module.exports = {
+    isEmpty,
+    stringClean,
+    spacesToUnderscore
 };
